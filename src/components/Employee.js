52a285c4512def745ccb6717c2fa13b472e2321d
Craig_Donavin <crdonavin@gmail.com>
2020-10-14 18:09:55 -0500

    add EMPLOYEE_QUERY and Employee const

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,11 +1,48 @@
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag'
-import UserAvatar from './UserAvatar';
+import EmployeeAvatar from './EmployeeAvatar';
 import Punches from './Punches';
+import { Fragment } from 'react';
 
 const EMPLOYEE_QUERY = gql`
     query Employee($id: ID!) {
         employee(id: $id) {
-            
+            punches {
+                id
+                clockIn
+                clockOut
+                todaysDate
+            }
         }
-    }`
+    }
+`
+
+const Employee = ({employee, selectEmployee}) => (
+    <Query query={EMPLOYEE_QUERY} variables={{ id: employee.id}}>
+        {({ loading, error, data }) => {
+            if (loading) return <div>Fetching...</div>
+            if (error) return <div>Error!</div>
+
+            return(
+                <Fragment>
+                    <div className='flex my-4'>
+                        <button
+                            className= 'bg-grey-light hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded'
+                            onClick = {selectEmployee.bind(this, null)}>
+                                Back
+                            </button>
+                    </div>
+                    <div className='flex mb-4'>
+                        <div className='my-4 w-1/4 rounded overflow-hidden'>
+                                <EmployeeAvatar employee={employee}/>
+                        </div>
+                        <div className='my-4 px-4 w-3/4'>
+                            <Punches punches={data.employee.punches} />
+                        </div>
+                    </div>
+                </Fragment>
+            )
+        }}
+    </Query>
+)
+export default Employee
