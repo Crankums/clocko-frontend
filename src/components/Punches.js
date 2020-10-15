1a5d7f0c019f426f9ee2551f2bd7ed115b223f45
Craig_Donavin <crdonavin@gmail.com>
2020-10-14 21:33:59 -0500

    add Punches component

---
+++ src/components/Punches.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react'
+
+const Punches = ({ punches }) => (
+    <Fragment>
+        {punches.map((punch) =>
+            <div key={punch.id} className='flex border-b border-solid border-grey-liight'>
+                <div className='w-3/4 p-4'>
+                    <h3>{punch.todaysDate}</h3>
+                    <p className='text-grey-darker'>
+                        "a group of attributes here"
+                    </p>
+                </div>
+                <div className='w-1/4 p-4 text-right'>
+                    "another attribute here"
+                </div>
+            </div>)}
+    </Fragment>
+);
+export default Punches
