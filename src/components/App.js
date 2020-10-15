a9623eb9e03c7e26d0c5f36d0bab6a5e4a724281
Craig_Donavin <crdonavin@gmail.com>
2020-10-14 21:43:49 -0500

    Add React to scope

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Employees from './Employees'
+import Employee from './Employee'
 
-function App() {
-  return (
-    <div className="container mx-auto px-4">
-      <Employees />
-    </div>
-  );
+class App extends Component  {
+  state = {
+    selectedEmployee: null
+  };
+
+  selectEmployee = (employee) => {
+    this.setState({ selectedEmployee: employee})
+  }
+  render(){
+    return (
+      <div className="container mx-auto px-4">
+        {this.state.selectedEmployee ?
+        <Employee employee={this.state.selectedEmployee} selectEmployee={this.selectEmployee} /> :
+        <Employees selectEmployee={this.selectEmployee} /> }
+      </div>
+    );
+  }
 }
 
 export default App;
