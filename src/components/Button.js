a04255d6076e1aee4d6f330044af819c07736acb
Craig_Donavin <crdonavin@gmail.com>
2020-12-08 23:27:50 -0600

    creates Button component

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import classnames from 'classnames'
+
+const Button = ({txt}) => {
+    
+    return (
+        <button className={classnames('bg-gray-100 shadow-md font-bold py-2 px-4 rounded')}>
+            {txt}
+        </button>
+    )
+}
+
+export default Button
