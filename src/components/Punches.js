8380095f676a9c7fc0d01fd11110b0e99f8231c3
Craig_Donavin <crdonavin@gmail.com>
2020-12-08 23:29:52 -0600

    adds Punches

--- src/components/Punches.js
+++ src/components/Punches.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react'
 const Punches = ({ punches }) => (
     <Fragment>
         {punches.map((punch) =>
-            <div key={punch.id} className='flex border-b border-solid border-grey-liight'>
+            <div key={punch.id} className='flex border-b border-solid border-grey-light'>
                 <div className='w-3/4 p-4'>
                     <h3>{punch.todaysDate}</h3>
                     <p className='text-grey-darker'>
