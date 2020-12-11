93f2b88aeed52f8e636f34337fb2df6980884427
Craig_Donavin <crdonavin@gmail.com>
2020-12-10 23:45:02 -0600

    removed Router wrapper

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,18 @@
 import React from 'react'
-import { 
-    BrowserRouter as Router,
-    Route,
-    Link,
-   } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <Router>
-            <div>
-                <nav className='navbar'>                      
-                    <Link to='/'>|Home| </Link>
-                    <Link to='/profile'> |Profile| </Link>
-                    <Link to='/time-clock'> |Time Clock| </Link>
-                    <Link to='/time-card'> |Time Card| </Link>
-                    <Link to='/admin-panel'> |Admin Panel| </Link>
-                    <Link to='/about'> |About| </Link>                   
-                </nav>
-            </div>
-        </Router>
+        <div>
+            <nav className='navbar'>                      
+                <Link to='/'>|Home| </Link>
+                <Link to='/profile'> |Profile| </Link>
+                <Link to='/time-clock'> |Time Clock| </Link>
+                <Link to='/time-card'> |Time Card| </Link>
+                <Link to='/admin-panel'> |Admin Panel| </Link>
+                <Link to='/about'> |About| </Link>                   
+            </nav>
+        </div>
     )
 }
 
