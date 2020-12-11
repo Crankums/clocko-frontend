d4b0622e2216b13de380d47ac3320164ff6086fb
Craig_Donavin <crdonavin@gmail.com>
2020-12-10 21:44:58 -0600

    fixed button styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import classnames from 'classnames'
 
-const Button = ({txt}) => {
+const Button = ({size, bgColor, txt}) => {
     
     return (
         <button className={classnames('bg-gray-100 shadow-md font-bold py-2 px-4 rounded')}>
