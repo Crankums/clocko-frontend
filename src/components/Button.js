1b3cddffa6b3ea1d1e3adafde6f9dc39ff81dddf
Craig_Donavin <crdonavin@gmail.com>
2020-12-10 21:52:08 -0600

    add margin to buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ import classnames from 'classnames'
 const Button = ({size, bgColor, txt}) => {
     
     return (
-        <button className={classnames('bg-gray-100 shadow-md font-bold py-2 px-4 rounded')}>
+        <button className={classnames('bg-gray-100 shadow-md font-bold py-2 px-4 rounded m-1')}>
             {txt}
         </button>
     )
